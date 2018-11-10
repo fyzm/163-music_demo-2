b336b973b7f94ce060503a58864d9a061b85ddb0
fyzm <fangyuan931018@gmail.com>
2018-11-10 15:52:40 +0800

    获取歌曲列表

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -2,9 +2,7 @@
   let view = {
     el:'#songList-container',
     template:`
-    <ul calss = "song in songs">
-      <li v-for="song in songs">
-      </li>
+    <ul class="songList">
     </ul>
     `,
     render(data){
@@ -17,6 +15,11 @@
         $el.find('ul').append(domLi)
       })
     },
+    activeItem(li){
+      let $li = $(li)
+      $li.addClass('active')
+        .siblings('.active').removeClass('active')
+    },
     clearActive(){
       $(this.el).find('.active').removeClass('active')
     }
@@ -24,6 +27,15 @@
   let model = {
     data:{
       songs:[]
+    },
+    find(){
+      var query = new AV.Query('Song');
+      return query.find().then((songs)=>{
+        this.data.songs = songs.map((song)=>{
+          return {id:song.id,...song.attributes}
+        })
+        return songs
+      })
     }
   }
   let controller = {
@@ -31,15 +43,28 @@
       this.view = view
       this.model = model
       this.view.render(this.model.data)
-      window.eventHub.on('upload',()=>{
-        this.view.clearActive()
-      })
-      window.eventHub.on('create',(songData)=>{
-        console.log(songData)
-        this.model.data.songs.push(songData)
-        this.view.render(this.model.data)
-      })
-    }
+      this.bindEvents()
+      this.getAllSongs()
+    }, 
+      getAllSongs(){
+        return this.model.find().then(()=>{
+          this.view.render(this.model.data)
+        })
+      },
+      bindEvents(){
+        $(this.view.el).on('click','li',(e)=>{
+          this.view.activeItem(e.currentTarget)
+        })
+      },
+      bindEventHub(){
+        window.eventHub.on('upload',()=>{
+          this.view.clearActive()
+        }),
+        window.eventHub.on('create',(songData)=>{
+          this.model.data.songs.push(songData)
+          this.view.render(this.model.data)
+        })
+      }
   }
   controller.init(view,model)
 }
