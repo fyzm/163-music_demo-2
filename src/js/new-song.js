bf824902e6aebd33a17d80b419edd385fa207310
fyzm <fangyuan931018@gmail.com>
2018-11-11 13:00:29 +0800

    增添编辑歌曲

--- src/js/new-song.js
+++ src/js/new-song.js
@@ -15,15 +15,20 @@
       this.model = model
       this.view.render(this.model.data)
       this.active()
+      window.eventHub.on('new',(data)=>{
+        this.active()
+      })
       window.eventHub.on('upload',(data)=>{
         this.active()
       })
       window.eventHub.on('select',(data)=>{
         this.deactive()
       })
+      $(this.view.el).on('click',this.active.bind(this))
     },
     active(){
       $(this.view.el).addClass('active')
+      window.eventHub.emit('new')
     },
     deactive(){
       $(this.view.el).removeClass('active')
